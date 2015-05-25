80f219451d0165f8d20b20e1d9f5270cbac71a80
Rakesh Dhote <rakesh.dhote@gmail.com>
2015-05-25 16:01:59 -0400

    Final Submission

--- Project-1_Script/Plot1_Histogram_rainVsNorain.py
+++ Project-1_Script/Plot1_Histogram_rainVsNorain.py
@@ -4,20 +4,25 @@ matplotlib.use('agg')
 import pandas as pd
 from ggplot import *
 
-def histplot_compare(datan):
+def histplot_compare(data):
 
-    df = pd.read_csv(datan)
-    xvar = df['ENTRIESn_hourly']
-    
-    gg = ggplot(aes(xvar, fill='rain', color='rain',label='rain'), data=df) \
-    + geom_histogram(alpha=0.6,binwidth = 250) + ggtitle("Histogram of ENTRIESn_hourly for rainy/nonrainy days") \
-    + labs("ENTRIESn_hourly", "Frequency")  #+ xlim(0, 5000)   
+    df = pd.read_csv(data)
+    drain1 = df[df.rain == 1]
+    dnorain = df[df.rain == 0]
+
+    gg = ggplot(df, aes('ENTRIESn_hourly')) + \
+    geom_histogram(drain1,fill = "red", alpha = 0.2, binwidth=250)+ \
+    geom_histogram(dnorain,fill = "blue", alpha = 0.2, binwidth=250) +\
+    xlim(0, 6000) +\
+    ylim(0, 8000) +\
+    ggtitle("Histogram of ENTRIESn_hourly for rainy/nonrainy days") \
+    + labs("ENTRIESn_hourly", "Frequency")
   
     return gg
 
 if __name__ == "__main__":
     data = "turnstile_weather_v2.csv"
-    image = "Histogram_RainVsNoRain22.png"
+    image = "Histogram_RainVsNoRain_bin250_Resubmit.png"
     gg =  histplot_compare(data)
     ggsave(image, gg, width=11, height=8)
 #    ggsave(image, gg)
